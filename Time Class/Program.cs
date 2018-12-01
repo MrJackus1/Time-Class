82858825edf76d91d70d411644544e0723a66417
MrJackus1 <44389248+MrJackus1@users.noreply.github.com>
2018-12-01 23:55:01 +0000

    Add project files.

---
+++ Time Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Time_Class;
+
+namespace Time_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Time is the type(class) and soft151Time is a new object(var)
+
+            Console.WriteLine("Please input the hour");
+
+            int h =(Convert.ToInt32(Console.ReadLine()));
+
+            Time soft151Time;
+            soft151Time = new Time(h);
+            
+            
+
+
+
+            soft151Time = new Time(16, 05, 00);
+
+            string output;
+            output = soft151Time.GetCurrentTime();
+
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Time Class/Time.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Class
+{
+    class Time
+    {
+        private int hours, mins, secs;
+
+
+         public Time(int inputHour)
+        {
+            hours = inputHour;
+
+        }
+
+
+
+
+        public Time(int theHour, int theMins, int theSecs)
+        {
+            hours = theHour;
+            mins = theMins;
+            secs = theSecs;
+        }
+
+
+
+        public string GetCurrentTime()
+        {
+            string output = "";
+
+            output = "Time is " + hours.ToString("00") + ":"
+                      + mins.ToString("00") + ":"
+                      + secs.ToString("00");
+
+
+            return output;
+        }
+
+        
+    }// End of Class
+}
